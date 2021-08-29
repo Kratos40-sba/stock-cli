f825c6455b0bb2f22da5479000f827f264d7e2a2
Kratos40-sba <a.khebchi@esi-sba.dz>
2021-08-29 01:52:10 +0100

    initial commit

---
+++ src/downloader.rs
@@ -0,0 +1,74 @@
+use std::error::Error;
+
+use crate::process_data::{min_and_max, Data};
+use chrono::{DateTime, Utc};
+use rust_decimal::prelude::*;
+use rust_decimal::Decimal;
+use std::sync::Arc;
+use yahoo_finance_api::{Quote, YahooConnector};
+
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+pub struct YQuote {
+    pub timestamp: u64,
+    pub open: Decimal,
+    pub high: Decimal,
+    pub low: Decimal,
+    pub volume: u64,
+    pub close: Decimal,
+    pub adjclose: Decimal,
+}
+
+impl From<Quote> for YQuote {
+    fn from(q: Quote) -> Self {
+        Self {
+            timestamp: q.timestamp,
+            open: Decimal::from_str(&q.open.to_string()).unwrap(), //todo could be done better - chop off X digits and go via int + currently using unwrap instead of proper deserialization
+            high: Decimal::from_str(&q.high.to_string()).unwrap(),
+            low: Decimal::from_str(&q.low.to_string()).unwrap(),
+            volume: q.volume,
+            close: Decimal::from_str(&q.close.to_string()).unwrap(),
+            adjclose: Decimal::from_str(&q.adjclose.to_string()).unwrap(),
+        }
+    }
+}
+
+///API Limits
+// - Using the Public API (without authentication), you are limited to 2,000 requests per hour per IP (or up to a total of 48,000 requests a day).
+// - Seems that 1h is resolution limit
+pub async fn fetch_stonks_data(
+    ticker: String,
+    from: DateTime<Utc>,
+    to: DateTime<Utc>,
+) -> Result<Data, Box<dyn Error>> {
+    println!("START downloading...");
+
+    let provider = YahooConnector::new();
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    //todo frequency - play with 1d/1h should work for both
+    let response = match provider
+        .get_quote_history_interval(&ticker, from, to, "1d")
+        .await
+    {
+        Ok(r) => r,
+        Err(e) => {
+            println!("An ERROR occured: {:?}", e);
+            return Err(Box::new(e));
+        }
+    };
+    let mut quotes: Vec<YQuote> = response
+        .quotes()
+        .unwrap()
+        .into_iter()
+        .map(|q| q.into())
+        .collect();
+
+
+
+    quotes.sort_by_cached_key(|k| k.timestamp); //just in case aren't sorted already
+
+    println!("END downloading...");
+
+    Ok(quotes)
+}
---
+++ src/main.rs
@@ -0,0 +1,154 @@
+use std::io;
+
+use chrono::{DateTime, TimeZone, Utc};
+use clap::Clap;
+use rust_decimal::prelude::*;
+use rust_decimal::Decimal;
+
+use async_std::prelude::*;
+use async_std::stream;
+use xactor::{message, Actor, Broker, Context, Handler, Result, Service, Supervisor};
+
+use future_finance_labs::download_data::fetch_stonks_data;
+use future_finance_labs::process_data::{
+    extract_adjclose, min_and_max, n_window_sma, price_diff, process_data, Data,
+};
+use std::time::Duration;
+
+//simpler but defo lacking functionality vs normal builder pattern
+//can't pass in Utc::now() as default value
+//can't pass in as const / static
+//had to the whole dance with default_value(x) and then correct as unwrap_or()
+#[derive(Clap)]
+#[clap(
+version = "0.1",
+author = "Kratos40-sba",
+about = "stock-cli app"
+)]
+struct Opts {
+    //Stonk tickers - lower or upper-case
+    #[clap(long, default_value = "AAPL,MSFT,UBER,GOOG")]
+    tickers: String,
+    ///Date in yyyy-mm-dd format. Default = 1 month ago.
+    #[clap(short, long, default_value = "x")]
+    from: String,
+    ///Date in yyyy-mm-dd format. Default = now.
+    #[clap(short, long, default_value = "x")]
+    to: String,
+}
+
+// ----------------------------------------------------------------------------- msg
+
+#[message]
+#[derive(Clone, Debug)]
+struct DownloadMsg {
+    ticker: String,
+    from: DateTime<Utc>,
+    to: DateTime<Utc>,
+}
+
+#[message]
+#[derive(Clone, Debug)]
+struct ProcessMsg {
+    data: Data,
+    ticker: String,
+}
+
+// ----------------------------------------------------------------------------- actor
+
+#[derive(Default)]
+struct DownloadActor;
+
+#[derive(Default)]
+struct ProcessActor;
+
+#[async_trait::async_trait]
+impl Actor for DownloadActor {
+    async fn started(&mut self, ctx: &mut Context<Self>) -> Result<()> {
+        ctx.subscribe::<DownloadMsg>().await;
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl Actor for ProcessActor {
+    async fn started(&mut self, ctx: &mut Context<Self>) -> Result<()> {
+        ctx.subscribe::<ProcessMsg>().await;
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl Handler<DownloadMsg> for DownloadActor {
+    async fn handle(&mut self, _ctx: &mut Context<Self>, msg: DownloadMsg) {
+        let data = fetch_stonks_data(msg.ticker.clone(), msg.from, msg.to)
+            .await
+            .unwrap();
+        //once Download Actor finishes its work, it publishes a msg to the next q, which is the processing q, to be picked up by processing actors
+        let _ = Broker::from_registry().await.unwrap().publish(ProcessMsg {
+            data,
+            ticker: msg.ticker,
+        });
+    }
+}
+
+#[async_trait::async_trait]
+impl Handler<ProcessMsg> for ProcessActor {
+    async fn handle(&mut self, _ctx: &mut Context<Self>, msg: ProcessMsg) {
+        process_data(msg.data, msg.ticker);
+    }
+}
+
+// ----------------------------------------------------------------------------- main
+
+#[xactor::main]
+async fn main() {
+    let opts = Opts::parse();
+    let from: DateTime<Utc> = opts
+        .from
+        .parse()
+        .unwrap_or(Utc::now() - chrono::Duration::days(60));
+    let to: DateTime<Utc> = opts.to.parse().unwrap_or(Utc::now());
+
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+    wtr.write_record(&[
+        "period start",
+        "symbol",
+        "price",
+        "change %",
+        "min",
+        "max",
+        "30d avg",
+    ])
+        .unwrap();
+    wtr.flush().unwrap();
+
+    // weird: if you don't collect addresses, the program stalls
+    // todo weird 2: if you start more than one actor - ALL of them get msgs
+    //  if this can't be fixed this solution is actually WORSE than my solution with tokio actors...
+    //  https://github.com/sunli829/xactor/issues/45
+    let _daddr = DownloadActor::start_default().await.unwrap();
+    // let _daddr2 = DownloadActor::start_default().await.unwrap();
+    let _paddr = ProcessActor::start_default().await.unwrap();
+    // let _paddr2 = ProcessActor::start_default().await.unwrap();
+
+    // their way - doesn't work for me, I don't see any msgs processed
+    // let downloader = Supervisor::start(|| DownloadActor);
+    // let processor = Supervisor::start(|| ProcessActor);
+    // let _ = downloader.join(processor).await;
+
+    // todo same story with the loop - if main isn't looping, actors won't have time to act
+    let mut interval = stream::interval(Duration::from_secs(10));
+    while interval.next().await.is_some() {
+        for ticker in opts.tickers.split(",").collect::<Vec<&str>>() {
+            // prep msg
+            let msg = DownloadMsg {
+                ticker: ticker.into(),
+                from,
+                to,
+            };
+            // send it
+            let _ = Broker::from_registry().await.unwrap().publish(msg);
+        }
+    }
+}
